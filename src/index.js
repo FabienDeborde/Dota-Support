2c5b35dcfaa13553e7f7d60f5db65a310a80bdce
Fabien <fabien.webdev@gmail.com>
2017-05-31 19:02:10 +0900

    Fix materialize import in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
